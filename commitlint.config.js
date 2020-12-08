ec67c52d1ec0093746996b650fdaf3da783dfa8f
whj <emailwen@qq.com>
2020-12-08 18:04:02 +0800

    build: 修改构建配置

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [2, 'always', [
-      "feat", "fix", "docs", "style", "refactor", "chore", "revert"
+      "feat", "fix", "docs", "style", "refactor", "chore", "revert", "build"
     ]],
     'subject-full-stop': [0, 'never'],
     'subject-case': [0, 'never']
